3ce10e154bca6080da3f2de8b5d2018b45592784
João <joaogabrielaraujodelima@gmail.com>
2024-03-25 19:54:27 -0300

    Modifying the phone and email field on Contact model.

--- contact/models.py
+++ contact/models.py
@@ -5,8 +5,8 @@ from django.utils import timezone
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
     phone = models.CharField(max_length=50)
-    phone = models.EmailField(max_length=254, blank=True)
+    email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
