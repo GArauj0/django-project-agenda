f37e7ac3c92c516296f339ecece14d823d18a5bc
João <joaogabrielaraujodelima@gmail.com>
2024-03-27 19:18:51 -0300

    Adding the Category model. Aggregating Category to Contact as foreign key.

--- contact/admin.py
+++ contact/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from contact.models import Contact
+from contact.models import Contact, Category
 
 # Register your models here.
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone',
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'category'
     ordering = '-id',
     search_fields = 'id', 'first_name', 'last_name'
     list_per_page = 10
     list_max_show_all = 200
     list_display_links = 'first_name',
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
