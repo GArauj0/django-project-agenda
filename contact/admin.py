b156ff4e80c8473f593a6005f148b4ab3fc36efc
João <joaogabrielaraujodelima@gmail.com>
2024-03-31 10:21:41 -0300

    Adding the single contact view for the Schedule and modifying the base template by adding a header.

--- contact/admin.py
+++ contact/admin.py
@@ -4,12 +4,13 @@ from contact.models import Contact, Category
 # Register your models here.
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone', 'category'
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'category', 'show'
     ordering = '-id',
     search_fields = 'id', 'first_name', 'last_name'
     list_per_page = 10
     list_max_show_all = 200
-    list_display_links = 'first_name',
+    list_display_links = 'id',
+    list_editable = 'first_name', 'last_name', 'show'
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
