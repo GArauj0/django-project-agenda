d7a23d2b063e5a68543a709b4201b205f57470f6
João <joaogabrielaraujodelima@gmail.com>
2024-04-03 16:32:20 -0300

    Adding search functionality.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
 
 # Create your views here.
@@ -7,6 +8,7 @@ def index(request):
 
     context = {
         'contacts' : contacts,
+        'site_title' : 'Contacts - '
     }
 
     return render(
@@ -15,11 +17,43 @@ def index(request):
         context
     )
 
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects\
+            .filter(show=True)\
+            .filter(
+                    Q(first_name__icontains=search_value) |
+                    Q(last_name__icontains=search_value) |
+                    Q(phone__icontains=search_value) |
+                    Q(email__icontains=search_value) |
+                    Q(id__icontains=search_value)
+            )\
+            .order_by('-id')
+
+    context = {
+        'contacts' : contacts,
+        'site_title' : 'Contacts - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
 def contact(request, contact_id):
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
     
     context = {
         'contact' : single_contact,
+        'site_title' : f'{single_contact.first_name} \
+                         {single_contact.last_name} - '
     }
 
     return render(
