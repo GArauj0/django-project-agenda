fd5637c36654049957be038ad7e3f7b5a4b6bae1
João <joaogabrielaraujodelima@gmail.com>
2024-04-04 16:13:11 -0300

    Adding pagination to Agenda.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from contact.models import Contact
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts' : contacts,
+        'page_obj' : page_obj,
         'site_title' : 'Contacts - '
     }
 
@@ -34,10 +39,15 @@ def search(request):
                     Q(id__icontains=search_value)
             )\
             .order_by('-id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts' : contacts,
-        'site_title' : 'Contacts - '
+        'page_obj' : page_obj,
+        'site_title' : 'Contacts - ',
+        'search_value' : search_value,
     }
 
     return render(
