d1722d986f9472cd0055f82da0d9accaeb7df9fc
João <joaogabrielaraujodelima@gmail.com>
2024-04-06 17:08:40 -0300

    Adding a validation method to the form field Phone.

--- contact/forms.py
+++ contact/forms.py
@@ -1,10 +1,27 @@
+from utils.validators import phone_validator
 from django import forms
 from . import models
+from django.core.exceptions import ValidationError
 
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = models.Contact
         fields = (
-            'first_name', 'last_name', 'phone',
+            'first_name', 'last_name', 'phone', 'email', 'description',
+            'category',
         )
+
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+
+        if not phone_validator.search(phone): # type: ignore
+            self.add_error(
+                'phone',
+                ValidationError(
+                    'This phone number is invalid.',
+                    code='invalid'
+                )
+            )
+
+        return phone
---
+++ utils/validators.py
@@ -0,0 +1,22 @@
+import re
+
+phone_validator = re.compile(r'((?:\(?\d{2}\)?\s*)?(?:9\s*)?(?:\d{4}-?\d{4}))')
+
+if __name__ == "__main__":
+
+    phones = """21912341234
+    (21)912341234
+    21 9 12341234
+    (21) 9 12341234
+    21912341234
+    (21)912341234
+    21 9 1234-1234
+    (21) 9 1234-1234
+    (21) 91234-1234
+    (21)9 1234-1234
+    """
+
+    for phone in phone_validator(phones): # type: ignore
+        print(phone)
+
+        
