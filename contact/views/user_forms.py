0212c266a0138740fbfa9b5a0977b42b99bf6269
João <joaogabrielaraujodelima@gmail.com>
2024-04-10 21:03:07 -0300

    Adding login and logout for users.

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,7 +1,8 @@
-from email import message
 from django.shortcuts import redirect, render
 from contact.forms import RegisterForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 
 def register(request):
@@ -13,7 +14,7 @@ def register(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'User registered successfully.')
-            redirect('user/register')
+            return redirect('contact:login')
 
     return render(
         request,
@@ -22,3 +23,28 @@ def register(request):
             'form': form,
         }
     )
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, 'You have successfully logged in.')
+            return redirect('contact:login')
+        messages.error(request, 'Invalid username/password.')
+
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form,
+        }
+    )
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
